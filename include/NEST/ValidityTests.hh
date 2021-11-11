aa8533e69364201ffce6cbda1531f9a59f0abd9d
Christopher Tunnell <ctunnell@nikhef.nl>
2021-11-11 12:50:55 -0600

    clang reformatting minus execNEST and Garfield

--- include/NEST/ValidityTests.hh
+++ include/NEST/ValidityTests.hh
@@ -1,14 +1,14 @@
 #ifndef VALIDITYTESTS_HH
 #define VALIDITYTESTS_HH 1
 
+#include <math.h>
 #include <cfloat>
-#include <math.h>  
 #include <iostream>
 using namespace std;
 
 class ValidityTests {
  public:
-  static bool nearlyEqual(double a, double b, double epsilon=1e-9);
+  static bool nearlyEqual(double a, double b, double epsilon = 1e-9);
 };
 
 #endif
